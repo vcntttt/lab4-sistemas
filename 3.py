c3849f6ebbe77a46eaa20a29145f9a454ab2b72f
Vicente Rivera <vicenterivera225@gmail.com>
2024-11-21 14:01:45 -0300

    avance en la 3

--- 3.py
+++ 3.py
@@ -1,8 +1,11 @@
 import pandas
 # from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 df = pandas.read_csv("Datas/body.csv")
-df = df[['bmxleg', 'bmxwaist']]
+df = df.dropna(subset=["bmxleg", 'bmxwaist'])
 
-
-print(df)
+plt.scatter(df['bmxleg'], df['bmxwaist'] , color="red")
+plt.xlabel("Circunferencia Cintura (bmxwaist)")
+plt.ylabel("Largo de Piernas (bmxleg)")
+plt.show()
